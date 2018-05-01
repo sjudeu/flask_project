5a338cc6c3ea1cc2884cd5220c4a8bbb8369eab0
Sjudeu <sjudeu2@gmail.com>
2018-05-01 16:39:43 +0000

    Adds alembic for database migration

---
+++ alembic/versions/3c39da5d71ca_create_users_table.py
@@ -0,0 +1,29 @@
+"""Create users table
+
+Revision ID: 3c39da5d71ca
+Revises: 
+Create Date: 2018-05-01 16:33:23.091321
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3c39da5d71ca'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+            "user",
+            sa.Column("id", sa.Integer, primary_key=True),
+            sa.Column("name", sa.String),
+            sa.Column("password", sa.String))
+
+
+def downgrade():
+    op.drop_table("user")
+
--- mdblog/models.py
+++ mdblog/models.py
@@ -6,3 +6,8 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     content = db.Column(db.String)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
