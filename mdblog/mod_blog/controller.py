a9bf3272f148e1245098a5b7a187cc61c562e7ce
Sjudeu <sjudeu2@gmail.com>
2018-05-17 12:47:24 +0000

    adds pagination

--- mdblog/mod_blog/controller.py
+++ mdblog/mod_blog/controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 
 from mdblog.models import Article
 
@@ -8,8 +9,11 @@ blog = Blueprint("blog", __name__)
 
 @blog.route("/articles/", methods=["GET"])
 def view_articles():
-    articles = Article.query.order_by(Article.id.desc()).all()
-    return render_template("mod_blog/articles.jinja", articles=articles)
+    page = request.args.get("page", 1, type=int)
+    paginate = Article.query.order_by(Article.id.desc()).paginate(page, 5, False)
+    return render_template("mod_blog/articles.jinja",
+            articles=paginate.items,
+            paginate=paginate)
 
 @blog.route("/articles/<int:art_id>/")
 def view_article(art_id):
